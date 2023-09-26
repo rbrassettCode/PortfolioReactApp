22b7dcebd546337012bda637794ce53de1d9c49a
rbrassettCode <rbrassett@outlook.com>
2023-09-26 14:30:34 -0400

    Installed AWS amplify CLI and imported backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "PortfolioReactApp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
