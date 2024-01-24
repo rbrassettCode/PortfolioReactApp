28e61361da6b89e80c52a0f24eabee2252f27303
rbrassettCode <rbrassett@outlook.com>
2024-01-23 23:07:17 -0500

    Converted Homepage to use react-bootstrap instead of raw react and css

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,23 +1,28 @@
 import React from "react";
 import './../Stylesheet.css';
+import { Col, Container, Row } from "react-bootstrap";
 
 function Contact() {
   return (
-    <div className="contact">
-      <div className="center-content">
-        <h2>Contact</h2>
-        <a href="mailto:rbrassett@outlook.com" target="_blank" className='link-text'>rbrassett@outlook.com</a>
-        <br />
-        <div className="center-content">
+    <Container className="pt-4">
+      <Row>
+        <Col >
+          <h2>Contact</h2>
+          <a href="mailto:rbrassett@outlook.com" target="_blank" className='link-text'>rbrassett@outlook.com</a>
+          <br />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
           <a href="https://www.linkedin.com/in/robert-brassett-9058a9177/" target="_blank" className='link-text'>
-            <i className="fab fa-linkedin social-icon"></i>
+              <i className="fab fa-linkedin social-icon"></i>
           </a>
           <a href="https://github.com/rbrassettCode" target="_blank" className='link-text'>
             <i className="fab fa-github social-icon"></i>
           </a>
-        </div>
-      </div>
-    </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 export default Contact;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,23 +2,30 @@ import React from 'react';
 import './../Stylesheet.css';
 import imageSrc from '../images/profile-pic.jpg'
 import Contact from './Contact';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 
 function HomePage() {
     return (
-      <div className='container'>
-        <div className='image-container'>
-          <img className='image-profile-pic' src={imageSrc} alt="profile-pic" />
-          <h2 className='image-text'>Hi there!</h2>
-        </div>
-        <br />
-        <div className='text-box'>
-          <p>I'm Robert Brassett, a software developer with a keen knack for crafting efficient software that addresses genuine challenges. With 3 years under my belt, I've extensively worked on backend systems, especially with Spring Boot. Java is my forte, but I'm a firm believer in continuous learning and regularly dive into new technologies to sharpen my skill set.</p>
-          <p>Beyond the digital realm, I cherish the great outdoors. Whether it's embarking on extensive hikes with my canine companion, scaling rocky terrains through rock climbing, or simply soaking in the local scenery on leisurely bike rides, these activities fuel my creativity and ensure I strike the right work-life balance. </p>
-          <p>Should you wish to dive deeper into my experience or brainstorm about a potential collaboration, I'm just a message away. Thanks for stopping by!</p>
-        </div>
-        <br />
-        <Contact />
-      </div>
+      <Container>
+        <Row >
+          <Col sm={12} md={4}>
+            <Image fluid roundedCircle src={imageSrc} alt="profile-pic" />
+          </Col>
+          <Col sm={12} md={4} className="d-flex align-items-end">
+            <h2>Hi there!</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col className='p-15 p-top-10'>
+            <p>I'm Robert Brassett, a software developer with a keen knack for crafting efficient software that addresses genuine challenges. With 3 years under my belt, I've extensively worked on backend systems, especially with Spring Boot. Java is my forte, but I'm a firm believer in continuous learning and regularly dive into new technologies to sharpen my skill set.</p>
+            <p>Beyond the digital realm, I cherish the great outdoors. Whether it's embarking on extensive hikes with my canine companion, scaling rocky terrains through rock climbing, or simply soaking in the local scenery on leisurely bike rides, these activities fuel my creativity and ensure I strike the right work-life balance. </p>
+            <p>Should you wish to dive deeper into my experience or brainstorm about a potential collaboration, I'm just a message away. Thanks for stopping by!</p>
+          </Col>
+        </Row>
+        <Row>
+          <Contact />
+        </Row>
+      </Container>
     );
   }
 
