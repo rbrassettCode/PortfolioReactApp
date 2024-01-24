f798510e6697214590c06cb398b4226ee61a18e6
rbrassettCode <rbrassett@outlook.com>
2024-01-24 00:05:19 -0500

    Fixed padding issues

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -16,7 +16,7 @@ function HomePage() {
           </Col>
         </Row>
         <Row>
-          <Col className='p-15 p-top-10'>
+          <Col className='pt-4'>
             <p>I'm Robert Brassett, a software developer with a keen knack for crafting efficient software that addresses genuine challenges. With 3 years under my belt, I've extensively worked on backend systems, especially with Spring Boot. Java is my forte, but I'm a firm believer in continuous learning and regularly dive into new technologies to sharpen my skill set.</p>
             <p>Beyond the digital realm, I cherish the great outdoors. Whether it's embarking on extensive hikes with my canine companion, scaling rocky terrains through rock climbing, or simply soaking in the local scenery on leisurely bike rides, these activities fuel my creativity and ensure I strike the right work-life balance. </p>
             <p>Should you wish to dive deeper into my experience or brainstorm about a potential collaboration, I'm just a message away. Thanks for stopping by!</p>
