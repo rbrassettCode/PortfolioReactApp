ded9710bb45a12e4ddff3961b4b35d866bc3edd8
rbrassettCode <rbrassett@outlook.com>
2024-02-22 14:36:12 -0500

    Formatted plot table better

--- src/pages/PlantMonitor.js
+++ src/pages/PlantMonitor.js
@@ -3,6 +3,7 @@ import { API } from "aws-amplify";
 import { listPlantStatuses } from "./../graphql/queries.js";
 import PlantChart from "./PlantChart";
 import './../Stylesheet.css';
+import { Container, Row, Col} from "react-bootstrap";
 
 function PlantMonitor() {
 
@@ -31,51 +32,59 @@ function PlantMonitor() {
         return list;
     }
     return (
-        <div>
-            <h2>Plant Data</h2>
-            <div className="plant-monitor">
-                <PlantChart data={allPlantData}/>
-            </div>
+        <Container>
+            <Row>
+                <h2>Plant Data</h2>
+            </Row>
+            <Row>
+                <Col className="col-12">
+                    <PlantChart data={allPlantData}/>
+                </Col>
+
+            </Row>
+           
             <br/>
-            <div className="plant-text-box ">
-                <h2>Welcome to my IoT garden project</h2>
+            <Row>
+                <Col className="text-start col-12">
+                    <h2>Welcome to my IoT garden project</h2>
+                </Col>
                 <br/>
-                <div className="plant-text">
-                    At the intersection of technology and nature, I've created a unique and innovative IoT project that brings the power of data and automation to your garden. My setup combines a Raspberry Pi, an Arduino Nano, AWS, and GraphQL to make your gardening experience smarter and more efficient.
-                </div>
-                <div className="plant-title">
-                    1. Soil Moisture Monitoring:
-                </div>
-                <div className="plant-text">
-                    I've implemented a cutting-edge soil moisture sensor using an Arduino Nano. It continuously measures the moisture levels in your garden soil to ensure your plants are getting just the right amount of water they need. 
-                </div>
-                <div className="plant-title">
-                    2. Intelligent Plant Watering:
-                </div>
-                <div className="plant-text">
-                    My system doesn't just monitor soil moisture; it also controls plant watering. When the soil moisture drops below a certain threshold, my setup automatically triggers the irrigation system, ensuring your plants are consistently and adequately hydrated.
-                </div>
-                <div className="plant-title">
-                    3. AWS Database Integration:
-                </div>
-                <div className="plant-text">
-                    I use Amazon Web Services (AWS) to store and manage the data collected from the sensors. This robust and scalable database solution keeps your garden data safe and easily accessible.
-                </div>
-                <div className="plant-title">
-                    4. Real-time Data Visualization:
-                </div>
-                <div className="plant-text">
-                    My website provides you with a real-time data visualization of your garden's soil moisture levels. You can monitor and track your garden's health from anywhere in the world.
-                </div>
-                <div className="plant-title">
-                    5. GraphQL Messaging:
-                </div>
-                <div className="plant-text">
-                    To seamlessly send and retrieve data, I've implemented GraphQL. This powerful query language allows efficient communication between your devices and the AWS database, ensuring data accuracy and speed.
+                <Col className="text-start col-12">
+                    <p>At the intersection of technology and nature, I've created a unique and innovative IoT project that brings the power of data and automation to your garden. My setup combines a Raspberry Pi, an Arduino Nano, AWS, and GraphQL to make your gardening experience smarter and more efficient.</p>
+                </Col>
+                <Col className="text-start col-12">
+                    <h2>1. Soil Moisture Monitoring:</h2>
+                </Col>
+                <Col className="text-start col-12">
+                    <p>I've implemented a cutting-edge soil moisture sensor using an Arduino Nano. It continuously measures the moisture levels in your garden soil to ensure your plants are getting just the right amount of water they need. </p>
+               </Col>
+                <Col className="text-start col-12">
+                    <h2>2. Intelligent Plant Watering:</h2>
+                </Col>
+                <Col className="text-start col-12">
+                    <p>My system doesn't just monitor soil moisture; it also controls plant watering. When the soil moisture drops below a certain threshold, my setup automatically triggers the irrigation system, ensuring your plants are consistently and adequately hydrated.</p>
+                </Col>
+                <Col className="text-start col-12">
+                    <h2>3. AWS Database Integration:</h2>
+                </Col>
+                <Col className="text-start col-12">
+                    <p>I use Amazon Web Services (AWS) to store and manage the data Collected from the sensors. This robust and scalable database solution keeps your garden data safe and easily accessible.</p>
+                </Col>
+                <Col className="text-start col-12">
+                    <h2>4. Real-time Data Visualization:</h2>
+                </Col>
+                <Col className="text-start col-12">
+                    <p>My website provides you with a real-time data visualization of your garden's soil moisture levels. You can monitor and track your garden's health from anywhere in the world.</p>
+                </Col>
+                <Col className="text-start col-12">
+                    <h2>5. GraphQL Messaging:</h2>
+                </Col>
+                <Col className="text-start col-12">
+                    <p>To seamlessly send and retrieve data, I've implemented GraphQL. This powerful query language allows efficient communication between your devices and the AWS database, ensuring data accuracy and speed.</p>
                     <br/>
-                </div>
-            </div>
-        </div>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
